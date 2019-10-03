79d37c2ae9287b5cf18a87beec2d34f4b61bc668
phuong0 <phuongunguyen4@gmail.com>
2019-10-03 01:00:49 +0000

    changed the name and color, added the code for taking the user's input and taking the first character, adding ay, taking the rest of the string, and bring the ay to the back and then print out the remainder of the string with ay at the end, still need to write the if else statement for the edge cases

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $( "document" ).ready(function() {
-
-
-
+    function seperate(userString) {
+        $("#button").click(function() {
+            var userInput = $(".input").val();
+            var firstChara = userInput.charAt(0);
+            var ay = firstChara + "ay";
+            var restCharas = userInput.substring(2);
+            $(".ouput").text(restCharas + ay);
+        });
+    }
 });
