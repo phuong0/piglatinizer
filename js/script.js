e045068700916737b44c88e58821d9fac4ef11c2
phuong0 <phuongunguyen4@gmail.com>
2019-10-03 01:38:00 +0000

    fixed previous code for pig latin

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 $( "document" ).ready(function() {
-    function seperate(userString) {
-        $("#button").click(function() {
-            var userInput = $(".input").val();
-            var firstChara = userInput.charAt(0);
-            var ay = firstChara + "ay";
-            var restCharas = userInput.substring(2);
-            $(".ouput").text(restCharas + ay);
-        });
-    }
+    $("#button").click(function(){
+        var userInput = $(".input").val();
+        var firstChara = userInput.charAt(0);
+        var ay = firstChara + "ay";
+        var restCharas = userInput.substring(1);
+        $(".output").append(restCharas + ay);
+     });
+
+
+
 });
